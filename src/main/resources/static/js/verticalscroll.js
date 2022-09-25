9378da52fb82ca205f95017bf39a69627fed76b1
mi1qw <123mib@gmail.com>
2022-09-26 01:18:18 +0300

    17

--- src/main/resources/static/js/verticalscroll.js
+++ src/main/resources/static/js/verticalscroll.js
@@ -2,7 +2,7 @@ const slideGallery = document.querySelector('.slides');
 const slides = slideGallery.querySelectorAll('div');
 const scrollbarThumb = document.querySelector('.thumb');
 const slideCount = slides.length;
-const slideHeight = 720;
+const slideHeight = 624;
 const marginTop = 16;
 
 const scrollThumb = () => {
